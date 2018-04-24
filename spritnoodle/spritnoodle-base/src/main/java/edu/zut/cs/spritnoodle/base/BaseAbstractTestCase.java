a9f28239a6535248660996cf8352f2093a59e7b8
octopus <569482018@qq.com>
2018-04-24 17:52:34 +0800

    add comment entity

---
+++ spritnoodle/spritnoodle-base/src/main/java/edu/zut/cs/spritnoodle/base/BaseAbstractTestCase.java
@@ -0,0 +1,5 @@
+package edu.zut.cs.spritnoodle.base;
+
+public class BaseAbstractTestCase {
+
+}
---
+++ spritnoodle/spritnoodle-base/src/main/java/edu/zut/cs/spritnoodle/base/domain/BaseMessageEntity.java
@@ -0,0 +1,34 @@
+package edu.zut.cs.spritnoodle.base.domain;
+
+
+public class BaseMessageEntity <T extends BaseMessageEntity<T>> extends BaseEntity {
+	private String message;
+	private String date;
+	private String author;
+	private String commenter;
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getCommenter() {
+		return commenter;
+	}
+	public void setCommenter(String commenter) {
+		this.commenter = commenter;
+	}
+	
+}
