6cca78222e1822a4f8962726fe403ae8f6a9836e
HEADS43 <kobe778899@yeah.net>
2018-06-06 22:35:58 +0800

    delete teaminformationmanager.java

--- Log-service/src/main/java/edu/zut/cs/javaee/log/admin/service/TeamInformationManager.java
+++
@@ -1,8 +0,0 @@
-package edu.zut.cs.javaee.log.admin.service;
-
-import edu.zut.cs.javaee.log.admin.domain.TeamInformation;
-import edu.zut.cs.javaee.log.base.service.GenericTreeManager;
-
-public interface TeamInformationManager extends GenericTreeManager<TeamInformation,Long>{
-
-}
