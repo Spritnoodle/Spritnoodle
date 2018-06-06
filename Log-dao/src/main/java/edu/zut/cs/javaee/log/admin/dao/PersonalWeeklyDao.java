dc3cf5f0a8814e2846ddfd55661ce3e59c1c452e
xuxianyong <848037236@qq.com>
2018-06-07 02:25:17 +0800

    update PersonalWeeklyDao.java

---
+++ Log-dao/src/main/java/edu/zut/cs/javaee/log/admin/dao/PersonalWeeklyDao.java
@@ -0,0 +1,8 @@
+package edu.zut.cs.javaee.log.admin.dao;
+
+import edu.zut.cs.javaee.log.admin.domain.PersonalWeekly;
+import edu.zut.cs.javaee.log.base.dao.GenericTreeDao;
+
+public interface PersonalWeeklyDao extends GenericTreeDao<PersonalWeekly, Long>{
+
+}
