d26b5c95b981c8a47df5fe99efa45635f38f9dad
HEADS43 <kobe778899@yeah.net>
2018-06-06 22:32:12 +0800

    delete the teaminformationdao

--- Log-dao/src/main/java/edu/zut/cs/javaee/log/admin/dao/TeamInformationDao.java
+++
@@ -1,8 +0,0 @@
-package edu.zut.cs.javaee.log.admin.dao;
-
-import edu.zut.cs.javaee.log.admin.domain.TeamInformation;
-import edu.zut.cs.javaee.log.base.dao.GenericTreeDao;
-
-public interface TeamInformationDao extends GenericTreeDao<TeamInformation, Long> {
-
-}
