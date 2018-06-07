0ba198be6eb62f240b33728fec89febeea68517c
HEADS43 <kobe778899@yeah.net>
2018-06-07 10:02:16 +0800

    Delete PersonalWeeklyDao.java

--- Log-dao/src/main/java/edu/zut/cs/javaee/log/admin/dao/PersonalWeeklyDao.java
+++
@@ -1,8 +0,0 @@
-package edu.zut.cs.javaee.log.admin.dao;
-
-import edu.zut.cs.javaee.log.admin.domain.PersonalWeekly;
-import edu.zut.cs.javaee.log.base.dao.GenericTreeDao;
-
-public interface PersonalWeeklyDao extends GenericTreeDao<PersonalWeekly, Long>{
-
-}
