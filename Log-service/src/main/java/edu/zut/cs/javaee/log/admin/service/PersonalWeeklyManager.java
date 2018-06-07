4bd44f12ac7bd82d99c760864f268b797dac5d48
HEADS43 <kobe778899@yeah.net>
2018-06-07 10:03:30 +0800

    Delete PersonalWeeklyManager.java

--- Log-service/src/main/java/edu/zut/cs/javaee/log/admin/service/PersonalWeeklyManager.java
+++
@@ -1,9 +0,0 @@
-package edu.zut.cs.javaee.log.admin.service;
-
-import edu.zut.cs.javaee.log.admin.domain.PersonalWeekly;
-import edu.zut.cs.javaee.log.base.service.GenericTreeManager;
-
-public interface PersonalWeeklyManager extends GenericTreeManager<PersonalWeekly,Long>{
-
-}
-
