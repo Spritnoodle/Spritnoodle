0f4213613ab423942fb32d286fe69c67dec6f3c0
xia-02 <37934817+xia-02@users.noreply.github.com>
2018-06-07 10:19:34 +0800

    Add files via upload

---
+++ Log-dao/src/main/java/edu/zut/cs/javaee/log/admin/dao/ModuleManagementDao.java
@@ -0,0 +1,13 @@
+
+package edu.zut.cs.javaee.log.admin.dao;
+
+import edu.zut.cs.javaee.log.admin.domain.ModuleManagement;
+import edu.zut.cs.javaee.log.base.dao.GenericTreeDao;
+
+
+public interface ModuleManagementDao extends GenericTreeDao<ModuleManagement,Long> {
+
+
+
+}
+
