3123387297c936986e9de1b1b4342feefc5ebcb5
zhouhuiquan1996 <37934203+zhouhuiquan1996@users.noreply.github.com>
2018-06-07 09:16:00 +0800

    Add files via upload

--- Log-service/src/test/java/edu/zut/cs/javaee/log/admin/service/PersonalMessageManagerTest.java
+++ Log-service/src/test/java/edu/zut/cs/javaee/log/admin/service/PersonalMessageManagerTest.java
@@ -1,5 +1,27 @@
 package edu.zut.cs.javaee.log.admin.service;
 
-public class PersonalMessageManagerTest {
+import org.springframework.beans.factory.annotation.Autowired;
+
+import edu.zut.cs.javaee.log.admin.domain.PersonalMessage;
+import edu.zut.cs.javaee.log.base.service.GenericTreeManagerTestCase;
+
+public class PersonalMessageManagerTest extends GenericTreeManagerTestCase<Long,PersonalMessage,PersonalMessageManager>{
+
+	
+	PersonalMessageManager personalmessageManager;
+	
+	
+	@Autowired
+	public void setPersonalMessageManager(PersonalMessageManager personalmessageManager) {
+		this.personalmessageManager = personalmessageManager;
+		this.manager=this.personalmessageManager;
+	}
+	
+
+
+	public PersonalMessageManagerTest() {
+		super(PersonalMessage.class);
+		// TODO Auto-generated constructor stub
+	}
 
 }
