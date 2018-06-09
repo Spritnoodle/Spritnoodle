bd3b4d49aeed4cba78e03370063cc6e1ff5386d4
xia-02 <37934817+xia-02@users.noreply.github.com>
2018-06-09 14:16:55 +0800

    Delete ModuleManagementManager.java

--- Log-service/src/main/java/edu/zut/cs/javaee/log/admin/service/ModuleManagementManager.java
+++
@@ -1,8 +0,0 @@
-package edu.zut.cs.javaee.log.admin.service;
-
-import edu.zut.cs.javaee.log.admin.domain.ModuleManagement;
-import edu.zut.cs.javaee.log.base.service.GenericTreeManager;
-
-public interface ModuleManagementManager extends GenericTreeManager<ModuleManagement,Long>{
-
-}
