09fafdd5eeb597ee1e7641460ff87f55a9879bea
xia-02 <37934817+xia-02@users.noreply.github.com>
2018-06-09 14:17:20 +0800

    Add files via upload

---
+++ Log-service/src/main/java/edu/zut/cs/javaee/log/admin/service/ModuleManagementManager.java
@@ -0,0 +1,8 @@
+package edu.zut.cs.javaee.log.admin.service;
+
+import edu.zut.cs.javaee.log.admin.domain.ModuleManagement;
+import edu.zut.cs.javaee.log.base.service.GenericTreeManager;
+
+public interface ModuleManagementManager extends GenericTreeManager<ModuleManagement,Long>{
+
+}
