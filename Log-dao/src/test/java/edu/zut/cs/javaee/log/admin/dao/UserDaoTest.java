c61d81e4f246d44c80fab39125a895375cdb6271
octopus <569482018@qq.com>
2018-06-27 16:11:23 +0800

    update UserDaoTest.java

--- Log-dao/src/test/java/edu/zut/cs/javaee/log/admin/dao/UserDaoTest.java
+++ Log-dao/src/test/java/edu/zut/cs/javaee/log/admin/dao/UserDaoTest.java
@@ -1,40 +1,31 @@
 package edu.zut.cs.javaee.log.admin.dao;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import edu.zut.cs.javaee.log.admin.dao.UserDao;
 import edu.zut.cs.javaee.log.admin.domain.User;
-import edu.zut.cs.javaee.log.base.dao.DaoConfig;
+import edu.zut.cs.javaee.log.base.dao.GenericDaoTestCase;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = DaoConfig.class)
-class UserDaoTest {
+import java.util.List;
+
+public class UserDaoTest extends GenericDaoTestCase<Long, User, UserDao> {
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger logger = LogManager.getLogger(UserDaoTest.class.getName());
 
 	@Autowired
 	UserDao userDao;
 
 	@Test
-	void test() {
-		String username = "world";
-		User query = new User();
-		// List<User> result = this.userDao.findOne(null);
+	public void testFindAll() {
 
-		User expectedUser = new User();
-		// expectedUser.setUsername(username);
-		// assertEquals(user, expectedUser);
-		// assertEquals(user.getUsername(), expectedUser.getUsername());
+		List<User> result = this.userDao.findAll();
+		if (logger.isInfoEnabled()) {
+			logger.info("testFindAll() - List<User> result={}", result); //$NON-NLS-1$
+		}
+
 	}
-	
-	/*
-	@Test
-	public void test() {
-		
-	}
-	*/
+
 }
