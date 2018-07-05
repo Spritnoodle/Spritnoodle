85a61e55eec82cd8dd7f06fa5efd19e5b50b38ca
octopus <569482018@qq.com>
2018-07-05 15:35:28 +0800

    update zhusi

--- Log-dao/src/main/java/edu/zut/cs/javaee/log/admin/dao/MessageDao.java
+++ Log-dao/src/main/java/edu/zut/cs/javaee/log/admin/dao/MessageDao.java
@@ -2,7 +2,11 @@ package edu.zut.cs.javaee.log.admin.dao;
 
 import edu.zut.cs.javaee.log.admin.domain.Message;
 import edu.zut.cs.javaee.log.base.dao.GenericTreeDao;
-
+/**
+ * 
+ * @author yifei
+ * 定义一个接口
+ */
 public interface MessageDao extends GenericTreeDao<Message, Long>{
 	
 }
--- Log-dao/src/main/java/edu/zut/cs/javaee/log/admin/domain/Message.java
+++ Log-dao/src/main/java/edu/zut/cs/javaee/log/admin/domain/Message.java
@@ -12,10 +12,17 @@ import javax.persistence.Table;
 
 import edu.zut.cs.javaee.log.base.domain.BaseTreeEntity;
 
+/**
+ * 
+ * @author yifei 用于建表的实体类
+ */
+// 数据库映射的表名
 @Table(name = "T_MESSAGE")
+// 这是一个实体bean
 @Entity
-@NamedQueries({@NamedQuery(name="Message.getRoot",query="select m from Message m where m.parent is null")})
-public class Message extends BaseTreeEntity<Message>{
+// 定义多个命名查询
+@NamedQueries({ @NamedQuery(name = "Message.getRoot", query = "select m from Message m where m.parent is null") })
+public class Message extends BaseTreeEntity<Message> {
 	/**
 	 * 
 	 */
@@ -23,7 +30,7 @@ public class Message extends BaseTreeEntity<Message>{
 	/**
 	 * 
 	 */
-	
+	// 对应表中的列名
 	@Column(name = "MESSAGE")
 	private String message;
 	@Column(name = "DATE")
@@ -32,29 +39,35 @@ public class Message extends BaseTreeEntity<Message>{
 	private String author;
 	@Column(name = "COMMENTER")
 	private String commenter;
-	
+
 	public String getMessage() {
 		return message;
 	}
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
-	
+
 	public String getDate() {
 		return date;
 	}
+
 	public void setDate(String date) {
 		this.date = date;
 	}
+
 	public String getAuthor() {
 		return author;
 	}
+
 	public void setAuthor(String author) {
 		this.author = author;
 	}
+
 	public String getCommenter() {
 		return commenter;
 	}
+
 	public void setCommenter(String commenter) {
 		this.commenter = commenter;
 	}
