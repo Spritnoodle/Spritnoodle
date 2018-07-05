7f71709c7247586db180ca85f6faf9e3f5b00ce8
octopus <569482018@qq.com>
2018-07-05 15:55:55 +0800

    add zhusi

--- Log-service/src/main/java/edu/zut/cs/javaee/log/admin/service/impl/MessageManagerImpl.java
+++ Log-service/src/main/java/edu/zut/cs/javaee/log/admin/service/impl/MessageManagerImpl.java
@@ -10,18 +10,24 @@ import edu.zut.cs.javaee.log.admin.domain.Message;
 import edu.zut.cs.javaee.log.admin.service.MessageManager;
 import edu.zut.cs.javaee.log.base.service.impl.GenericTreeManagerImpl;
 
+/**
+ * 
+ * @author yifei
+ *
+ */
+// 实例化MessageManager
 @Service("messageManager")
+// 声明事务
 @Transactional
-public class MessageManagerImpl extends GenericTreeManagerImpl<Message,Long> implements MessageManager{
-	
+public class MessageManagerImpl extends GenericTreeManagerImpl<Message, Long> implements MessageManager {
+
 	MessageDao messageDao;
 
 	@Autowired
 	public void setMessageDao(MessageDao messageDao) {
 		this.messageDao = messageDao;
-		this.treeDao=this.messageDao;
-		this.dao=this.treeDao;
+		this.treeDao = this.messageDao;
+		this.dao = this.treeDao;
 	}
-	
-	
+
 }
